82196c171bfb924f6aed2451c08f3e4768bd8233
Julie P <paupertjulie@gmail.com>
2020-12-08 18:03:07 +0100

    challenge Mars Lander

--- Mars Lander (easy).js
+++ Mars Lander (easy).js
@@ -6,4 +6,11 @@ for (var i = 0; i < N; i++) {
 
 while (true) {
     console.log(readline().split(' ')[3] < -39 ? '0 4' : '0 0');
+
+    // OU
+    if (readline().split(' ')[3] < -39) {
+        console.log('O 4');
+    } else {
+        console.log('0 0');
+    }
 };

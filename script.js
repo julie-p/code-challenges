dc222869e4f515c8154a068305b63ba418b102e5
Julie P <paupertjulie@gmail.com>
2020-12-08 14:22:53 +0100

    first challenge - the descent

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-/* Objectif
+/* 
+Objectif :
 Écrivez le programme permettant de détruire les montagnes pour pouvoir attérir. Pour cela, tirez sur la montagne la plus haute.
- 	Règles
+Règles :
 Au début de chaque tour de jeu, vous recevez en entrée la hauteur de chaque montagne de gauche à droite.
 Avant la fin du tour de jeu, vous devez indiquer la montagne la plus haute pour tirer dessus.
 
-Tirer sur une montagne ne fera qu'en détruire une partie. Votre vaisseau descend à chaque passe. */
+Tirer sur une montagne ne fera qu'en détruire une partie. Votre vaisseau descend à chaque passe. 
+*/
 
 // Récupérer la montagne la plus haute 
 // Récupérer la montagne à détruire
